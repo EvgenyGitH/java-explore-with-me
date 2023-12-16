c3795b77792bf7640232263ca9a7ddf04f175e5b
Evgeny <e-walker@yandex.ru>
2023-12-17 02:59:55 +0300

    fix: checkStyle

--- ewm-stats-service/stats-service/src/main/java/ru/practicum/statsservice/controller/StatsController.java
+++ ewm-stats-service/stats-service/src/main/java/ru/practicum/statsservice/controller/StatsController.java
@@ -1,6 +1,5 @@
 package ru.practicum.statsservice.controller;
 
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.annotation.DateTimeFormat;
